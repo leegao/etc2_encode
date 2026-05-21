e476aef7ef147fac65252cd42f31f3e9ee5bfdd1
leegao <lg342@cornell.edu>
2026-05-21 11:42:38 +0800

    Make ENABLE_DIAGNOSTICS a compiler flag

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 echo "Compiling compress.slang to compress.comp (GLSL)"
-slangc compress.slang -target glsl > compress.comp
+slangc compress.slang -target glsl -D ENABLE_DIAGNOSTICS > compress.comp
 
 echo "Compiling compress.slang to compress.spv (SPIR-V)"
-slangc compress.slang -profile glsl_450 -target spirv -o compress.spv -entry main
+slangc compress.slang -profile glsl_450 -target spirv -o compress.spv -entry main -D ENABLE_DIAGNOSTICS
 spirv-dis compress.spv > compress.spvasm
 
 echo "Compiling compress.comp to SPIR-V"
