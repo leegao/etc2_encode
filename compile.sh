149e78798c9fce3400b171d210699bd78a488a75
leegao <lg342@cornell.edu>
2026-05-21 11:35:01 +0800

    Add spv disassem

--- compile.sh
+++ compile.sh
@@ -1,7 +1,9 @@
 echo "Compiling compress.slang to compress.comp (GLSL)"
 slangc compress.slang -target glsl > compress.comp
+
 echo "Compiling compress.slang to compress.spv (SPIR-V)"
 slangc compress.slang -profile glsl_450 -target spirv -o compress.spv -entry main
+spirv-dis compress.spv > compress.spvasm
 
 echo "Compiling compress.comp to SPIR-V"
 glslangValidator -V compress.comp -x -o compress.inc
