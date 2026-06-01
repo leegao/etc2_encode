1252ecd1bded8e895a963b2dc5150fdf4245592a
leegao <lg342@cornell.edu>
2026-06-02 00:30:55 +0800

    Optimal 1P ASTC encoding
    ASTC: PSNR: 32.4673, 0.0887 ms
    ETC2: PSNR: 29.2100, 0.2294 ms
    
    2.12x improvement in quality

--- astc_enc.sh
+++ astc_enc.sh
@@ -1,9 +1,9 @@
 # echo "Compiling compress.slang to compress.comp (GLSL)"
-# slangc compress.slang -target glsl -line-directive-mode none -D ENABLE_DIAGNOSTICS1 -D DISABLE_RECONSTRUCTION1 > compress.comp
+slangc astc_enc.slang -target glsl -line-directive-mode none -D ENABLE_DIAGNOSTICS1 -D DISABLE_RECONSTRUCTION1 > astc_enc.comp
 
 echo "Compiling astc_enc.slang to astc_enc.spv (SPIR-V)"
 slangc astc_enc.slang -profile glsl_450 -target spirv -o astc_enc.spv -D ENABLE_DIAGNOSTICS -D DISABLE_RECONSTRUCTION1 -entry main
-# spirv-dis compress.spv > compress.spvasm
+spirv-dis astc_enc.spv > astc_enc.spvasm
 
 echo "Compiling astc_enc.comp to SPIR-V"
 # glslangValidator -V compress.comp -x -o compress.inc
