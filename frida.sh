db679be9def9adca9d20418fb11d2fb852cd02ca
leegao <lg342@cornell.edu>
2026-06-14 00:53:08 +0800

    Use a varying-based (no-LDS) optimization barrier to avoid unrolling solve_weights
    Approach, use the uniform constant (push constant) which allows using
    the uniform register

--- frida.sh
+++ frida.sh
@@ -1,6 +1,6 @@
-slangc astc_enc.slang -target glsl -line-directive-mode none -D DISABLE_RECONSTRUCTION > astc_enc.comp
+slangc astc_enc.slang -target glsl -line-directive-mode none -D DISABLE_RECONSTRUCTION -D dont_UNROLL_SOLVE_WEIGHTS > astc_enc.comp
 rm astc_enc_mbs2_shader.h
-frida -f ~/Downloads/Arm_Performance_Studio_2026.2/mali_offline_compiler/malioc -l hook.js -- -c Mali-G615 --vulkan astc_enc.comp
+frida -f ~/Downloads/Arm_Performance_Studio_2026.2/mali_offline_compiler/malioc -l hook.js -- -d -c Mali-G615 --vulkan astc_enc.comp
 
 gcc disassemble_astc_enc.c disassemble.c -o disassemble_astc_enc.out
 ./disassemble_astc_enc.out
