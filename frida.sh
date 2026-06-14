c523ebb1fa600a3fe3bb6fde1c1d4eda373b0edc
leegao <lg342@cornell.edu>
2026-06-14 13:51:03 +0800

    Add debug info

--- frida.sh
+++ frida.sh
@@ -1,4 +1,4 @@
-slangc astc_enc.slang -target glsl -line-directive-mode none -D DISABLE_RECONSTRUCTION -D dont_UNROLL_SOLVE_WEIGHTS > astc_enc.comp
+slangc astc_enc.slang -target glsl -line-directive-mode none -D DISABLE_RECONSTRUCTION -D dont_UNROLL_SOLVE_WEIGHTS -D ENABLE_DEBUG_LINE_INFO > astc_enc.comp
 rm astc_enc_mbs2_shader.h
 frida -f ~/Downloads/Arm_Performance_Studio_2026.2/mali_offline_compiler/malioc -l hook.js -- -d -c Mali-G615 --vulkan astc_enc.comp
 
